5c616f74387814b8797612fdfed8ac34b5ca44cd
Raunak Sethiya <sethiyark@gmail.com>
2018-07-29 23:08:32 +0530

    Add files via upload

---
+++ Wifi_WebLogin_Multi/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(72, 49)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox2.Location = New System.Drawing.Point(72, 75)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 52)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 75)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Button1.Location = New System.Drawing.Point(212, 52)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 39)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(295, 46)
+        Me.WebBrowser1.TabIndex = 5
+        Me.WebBrowser1.Url = New System.Uri("", System.UriKind.Relative)
+        Me.WebBrowser1.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Viner Hand ITC", 6.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(209, 94)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "By Raunak Sethiya"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Kristen ITC", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(5, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(289, 18)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Please Submit Username and Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ClientSize = New System.Drawing.Size(295, 109)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Name = "Form1"
+        Me.Text = "Wifi WebLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Wifi_WebLogin_Multi/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+    Dim i As Integer = 1
+    Dim pageready As Boolean = False
+    Dim username As String = ""
+    Dim password As String = ""
+    Private Sub pagewait(ByVal sender As Object, ByVal e As WebBrowserDocumentCompletedEventArgs)
+        If WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            pageready = True
+            RemoveHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf pagewait)
+        End If
+    End Sub
+    Private Sub waitforpageload()
+        AddHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf pagewait)
+        While Not pageready
+            Application.DoEvents()
+        End While
+        pageready = False
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        username = TextBox1.Text
+        password = TextBox2.Text
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+    Private Sub Input_Username()
+        AddHandler TextBox1.TextChanged, New EventHandler(AddressOf TextBox1_TextChanged)
+        Application.DoEvents()
+        AddHandler TextBox1.TextChanged, New EventHandler(AddressOf TextBox2_TextChanged)
+        Application.DoEvents()
+        AddHandler Button1.Click, New EventHandler(AddressOf Button1_Click)
+        Application.DoEvents()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i As Integer = 0 To 2
+            i = 1
+            If username = "" Or password = "" Then
+                Me.Visible = True
+                Input_Username()
+            Else
+                Me.Visible = False
+                Dim Test1 As Integer = 0
+                Dim Test2 As Integer = 0
+                Dim Test3 As Integer = 0
+                Dim Test4 As Integer = 0
+
+                On Error Resume Next
+                If My.Computer.Network.Ping("www.google.com") = True Then
+                    Test1 = 1
+                ElseIf My.Computer.Network.Ping("www.google.com") = False Then
+                    Test1 = 0
+                End If
+
+                If My.Computer.Network.Ping("www.yahoo.com") = True Then
+                    Test2 = 1
+                ElseIf My.Computer.Network.Ping("www.yahoo.com") = False Then
+                    Test2 = 0
+                End If
+
+                If My.Computer.Network.Ping("www.bing.com") = True Then
+                    Test3 = 1
+                ElseIf My.Computer.Network.Ping("www.bing.com") = False Then
+                    Test3 = 0
+                End If
+
+                Test4 = Test1 + Test2 + Test3
+
+                If Test4 > 0 Then
+                    MsgBox("Internernet Available")
+
+                Else
+                    'MsgBox("Authentication Required")
+                    WebBrowser1.Navigate("https://1.1.1.1/login.html")
+                    waitforpageload()
+                    WebBrowser1.ScriptErrorsSuppressed = True
+                    Dim j As Integer = 1
+                    For j = 0 To 2
+                        j = 1
+
+                        If WebBrowser1.Document.Title = "Web Authentication" Then
+                            Dim check As HtmlElement = WebBrowser1.Document.GetElementById("Submit")
+                            If check IsNot Nothing Then
+                                j = 3
+
+                            Else
+                                WebBrowser1.Navigate("https://1.1.1.1/login.html")
+                                waitforpageload()
+
+                            End If
+                        Else
+                            WebBrowser1.Navigate("https://1.1.1.1/login.html")
+                            waitforpageload()
+
+                        End If
+                    Next
+                    j = 1
+
+                    WebBrowser1.Document.GetElementById("username").SetAttribute("value", username)
+                    WebBrowser1.Document.GetElementById("password").SetAttribute("value", password)
+                    WebBrowser1.Document.GetElementById("Submit").InvokeMember("click")
+
+                End If
+            End If
+
+            System.Threading.Thread.Sleep(1000)
+
+        Next
+    End Sub
+End Class
+
